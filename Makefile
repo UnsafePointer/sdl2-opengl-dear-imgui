7ad4471e8d5fee35f71ea760c97f3f337630fc58
Renzo Crisostomo <renzo@crisostomo.me>
2019-07-20 15:37:38 +0200

    Add Dear ImGui

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 CXX		  := g++
-CXX_FLAGS := -Wall -Wextra -std=c++17 -ggdb `sdl2-config --cflags`
+CXX_FLAGS := -Wall -Wextra -std=c++17 -ggdb `sdl2-config --cflags` -DIMGUI_IMPL_OPENGL_LOADER_GLAD
 
 OPENGL_API := 4.5
 GLAD_BUILD_DIR := glad
+
+IMGUI_DIR := imgui
+IMGUI_SRC := $(IMGUI_DIR)/examples/imgui_impl_sdl.cpp $(IMGUI_DIR)/examples/imgui_impl_opengl3.cpp
+IMGUI_SRC += $(IMGUI_DIR)/imgui.cpp $(IMGUI_DIR)/imgui_demo.cpp $(IMGUI_DIR)/imgui_draw.cpp $(IMGUI_DIR)/imgui_widgets.cpp
 
 BIN		:= bin
 SRC		:= src
@@ -22,8 +26,8 @@ run: clean all
 $(GLAD_BUILD_DIR)/src/*.c:
 	python -m glad --out-path=$(GLAD_BUILD_DIR) --api="gl=$(OPENGL_API)" --extensions="GL_KHR_debug" --generator="c"
 
-$(BIN)/$(EXECUTABLE): $(SRC)/*.cpp $(GLAD_BUILD_DIR)/src/*.c
-	$(CXX) $(CXX_FLAGS) -I$(INCLUDE) -I$(GLAD_BUILD_DIR)/include -L$(LIB) $^ -o $@ $(LIBRARIES)
+$(BIN)/$(EXECUTABLE): $(SRC)/*.cpp $(GLAD_BUILD_DIR)/src/*.c $(IMGUI_SRC)
+	$(CXX) $(CXX_FLAGS) -I$(INCLUDE) -I$(GLAD_BUILD_DIR)/include -I$(IMGUI_DIR) -I$(IMGUI_DIR)/examples -L$(LIB) $^ -o $@ $(LIBRARIES)
 
 clean:
 	rm -rf $(BIN)/*
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 #include <glad/glad.h>
+#include "imgui.h"
+#include "examples/imgui_impl_sdl.h"
+#include "examples/imgui_impl_opengl3.h"
 
 const uint32_t WIDTH = 1024;
 const uint32_t HEIGHT = 768;
@@ -22,7 +25,15 @@ int main() {
         exit(1);
     }
     cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << endl;
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplSDL2_InitForOpenGL(window, glContext);
+    ImGui_ImplOpenGL3_Init();
+    ImVec4 background = ImVec4(35/255.0f, 35/255.0f, 35/255.0f, 1.00f);
+
     bool quit = false;
     while (!quit) {
         SDL_Event event;
@@ -31,9 +42,60 @@ int main() {
                 quit = true;
             }
         }
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame(window);
+        ImGui::NewFrame();
+
+        {
+            static int counter = 0;
+            int sdl_width = 0, sdl_height = 0, controls_width = 0;
+            SDL_GetWindowSize(window, &sdl_width, &sdl_height);
+            controls_width = sdl_width;
+            if ((controls_width /= 3) < 300) {
+                controls_width = 300;
+            }
+
+            ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_Always);
+            ImGui::SetNextWindowSize(
+                ImVec2(static_cast<float>(controls_width), static_cast<float>(sdl_height - 20)),
+                ImGuiCond_Always
+                );
+
+            ImGui::Begin("Controls", NULL, ImGuiWindowFlags_NoResize);
+
+            ImGui::Dummy(ImVec2(0.0f, 1.0f));
+            ImGui::TextColored(ImVec4(1.0f, 0.0f, 1.0f, 1.0f), "Platform");
+            ImGui::Text("%s", SDL_GetPlatform());
+            ImGui::Text("CPU cores: %d", SDL_GetCPUCount());
+            ImGui::Text("RAM: %.2f GB", SDL_GetSystemRAM() / 1024.0f);
+            if (ImGui::Button("Counter button")) {
+                std::cout << "counter button clicked\n";
+                counter++;
+            }
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::End();
+        }
+
+        ImGui::Render();
+        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+        glClearColor(
+            background.x,
+            background.y,
+            background.z,
+            background.w
+            );
         glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         SDL_GL_SwapWindow(window);
     }
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_GL_DeleteContext(glContext);
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
